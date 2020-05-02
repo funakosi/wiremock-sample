75b195c98476ba935288697db81c53bca1c939be
Akira.Funakoshi <funakoshi@plusknowledge.co.jp>
2020-05-02 11:46:20 +0900

    - Rest Assured を使ったサンプルを追加 - Bodyの文字列にファイルを参照するサンプルを追加

--- src/test/java/com/github/aibou/samples/MyHttpClientTest.java
+++ src/test/java/com/github/aibou/samples/MyHttpClientTest.java
@@ -1,16 +1,21 @@
 package com.github.aibou.samples;
 
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Test;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.net.SocketTimeoutException;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import io.restassured.RestAssured;
 
 public class MyHttpClientTest {
     @ClassRule
@@ -43,34 +48,61 @@ public class MyHttpClientTest {
                         aResponse().withStatus(200)
                                 .withFixedDelay(2000)
                 ));
+
+        stubFor(get(urlEqualTo("/body-file"))
+                .willReturn(aResponse()
+                        .withBodyFile("body.json")));
     }
 
     @Test
     public void returns_1000_when_status_is_200() throws IOException {
         MyHttpClient client = new MyHttpClient();
         assertThat(client.fetchStatus("http://localhost:8089/200"), is(1000));
+        //another example
+        RestAssured.baseURI = "http://localhost:8089/200";
+        given().get().then().statusCode(200);
     }
 
     @Test
     public void returns_1510_when_status_is_302() throws IOException {
         MyHttpClient client = new MyHttpClient();
         assertThat(client.fetchStatus("http://localhost:8089/302"), is(1510));
+        //another example
+        RestAssured.baseURI = "http://localhost:8089/302";
+        given().get().then().statusCode(302);
     }
 
     @Test
     public void returns_2005_when_status_is_401() throws IOException {
         MyHttpClient client = new MyHttpClient();
         assertThat(client.fetchStatus("http://localhost:8089/401"), is(2005));
+        //another example
+        RestAssured.baseURI = "http://localhost:8089/401";
+        given().get().then().statusCode(401);
     }
 
     @Test
     public void returns_2500_when_status_is_500() throws IOException {
         MyHttpClient client = new MyHttpClient();
         assertThat(client.fetchStatus("http://localhost:8089/500"), is(2500));
+        //another example
+        RestAssured.baseURI = "http://localhost:8089/500";
+        given().get().then().statusCode(500);
     }
 
     @Test(expected = SocketTimeoutException.class)
     public void タイムアウト() throws IOException {
         new MyHttpClient().fetchStatus("http://localhost:8089/timeout");
+        //another example
+        RestAssured.baseURI = "http://localhost:8089/timeout";
+        given().get().then().statusCode(200);
+    }
+
+    @Test
+    public void returns_json() {
+        RestAssured.baseURI = "http://localhost:8089/body-file";
+        given().get().then().statusCode(200);
+        String response = given().get().asString();
+        System.out.println(response);
     }
 }
